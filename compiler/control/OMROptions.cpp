b38638c6447cd62ba474b76214ae09d1c6f554b7
Henry Zongaro <zongaro@ca.ibm.com>
2026-09-06 05:14:14 -0700

    Add tracing of setting of TR_NoOptServer

--- compiler/control/OMROptions.cpp
+++ compiler/control/OMROptions.cpp
@@ -5979,6 +5979,8 @@ void OMR::Options::setConservativeQuickStart()
 {
     self()->setQuickStart();
     self()->setOption(TR_NoOptServer);
+    TR_VerboseLog::writeLineLocked(TR_Vlog_INL,
+        "(1) In OMR::Options::setConservativeQuickStart - setting options.getOption(TR_NoOptServer) to true\n");
     // more conservative hot thresholds (already enabled on big apps)
     _sampleThreshold = 1000; // 3%
 
@@ -6090,6 +6092,8 @@ void OMR::Options::setConservativeDefaultBehavior()
 
     self()->setOption(TR_DisableGuardedCountingRecompilations);
     self()->setOption(TR_NoOptServer);
+    TR_VerboseLog::writeLineLocked(TR_Vlog_INL,
+        "(1) In OMR::Options::setConservativeDefaultBehaviour - setting options.getOption(TR_NoOptServer) to true\n");
     self()->setOption(TR_ConservativeCompilation, true);
     // conservative upgrades ?
     _coldUpgradeSampleThreshold = 30; // instead of 3 or even 2
