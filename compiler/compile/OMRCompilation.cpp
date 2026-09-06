56ac0a36d5a7d4c6d368f9a8423db52783313c21
Henry Zongaro <zongaro@ca.ibm.com>
2026-09-06 06:57:52 -0700

    Add tracing of setting of TR_NoOptServer

--- compiler/compile/OMRCompilation.cpp
+++ compiler/compile/OMRCompilation.cpp
@@ -353,7 +353,7 @@ OMR::Compilation::Compilation(int32_t id, OMR_VMThread *omrVMThread, TR_FrontEnd
 #endif
         );
     _isServerInlining = !options.getOption(TR_NoOptServer);
-    if (options->getVerboseOption(TR_VerboseInlining)) {
+    if (options.getVerboseOption(TR_VerboseInlining)) {
         TR_VerboseLog::writeLineLocked(TR_Vlog_INL,
             "In OMR::Compilation::Compilation - setting _isServerInlining == %; options.getOption(TR_NoOptServer) "
             "== %d\n",
